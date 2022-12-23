558048c00e80da401fc4a7c1f296cc3dbc2a24b8
Sumeyye <sumeyyedagdevirozmen@gmail.com>
2022-12-23 21:09:26 +0000

    23/12/22

--- src/test/java/day04_Maven/Practice03.java
+++ src/test/java/day04_Maven/Practice03.java
@@ -7,11 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Practice03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -24,16 +25,33 @@ public class Practice03 {
 
         //    ~ get the brandName of phones
 
-        List<WebElement> brandName=  driver.findElements(By.xpath("//title"));
-        for(WebElement w:brandName){
+        List<WebElement> brandNames=  driver.findElements(By.xpath("//h4"));
+        for(WebElement w:brandNames){
             System.out.println(w.getText());
         }
-
+        System.out.println("-------------------------------");
 
         //    ~ click on add to button for all elements
+        List<WebElement> buttonForAllElements = driver.findElements(By.xpath("(//*[text()='Add to Cart'])"));
+        for(WebElement w:buttonForAllElements){
+            w.click();
+        }
+
         //    ~ click on black total added cart button
+        driver.findElement(By.id("cart-total")).click();
+
         //    ~ get the names of list from the cart
+       List<WebElement> namesOfList = driver.findElements(By.xpath("//*[@class='text-left']"));
+       for(WebElement w:namesOfList){
+           System.out.println(w.getText());
+       }
+
         //    ~ compare the names from displaying list and cart list
-        //
+        if(brandNames.equals(namesOfList)){
+            System.out.println("Test PASSED");
+        }else System.out.println("Test FAILED");
+       // close the driver
+        Thread.sleep(3000);
+        driver.close();
     }
 }
