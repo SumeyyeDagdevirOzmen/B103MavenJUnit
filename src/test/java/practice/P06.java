125e879da37568b2bca2c2b64e15c783c0421040
Sumeyye <sumeyyedagdevirozmen@gmail.com>
2022-12-27 22:16:30 +0000

    27/12/22

--- src/test/java/practice/P06.java
+++ src/test/java/practice/P06.java
@@ -1,14 +1,38 @@
 package practice;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class P06 {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         //https://id.heroku.com/login sayfasina gidin
+        driver.get("https://id.heroku.com/login ");
         //Bir mail adersi giriniz
+        driver.findElement(By.xpath("//*[@id='onetrust-accept-btn-handler']")).click();//cookies
+
+        driver.findElement(By.xpath("//*[@id='email']")).sendKeys("sumeyyedag@yahoo.com");
         //Bir password giriniz
+        driver.findElement(By.xpath("//*[@id='password']")).sendKeys("1234567!");
         //Login butonuna tiklayiniz
+        driver.findElement(By.xpath("//*[text()='Log In']")).click();
         //"There was a problem with your login." texti gorunur ise
         //"kayit yapilamadi" yazdiriniz
         //eger yazi gorunur degilse "kayit yapildi" yazdiriniz
+        WebElement yazi = driver.findElement(By.xpath("//div[@class='alert alert-danger']"));
+       String result= yazi.isDisplayed() ? "Kayit Yapilamadi" : "Kayit Yapildi";
+        System.out.println(result);
+
+
         //sayfayı kapatiniz
+        driver.close();
     }
 }
--- src/test/java/practice/P07.java
+++ src/test/java/practice/P07.java
@@ -1,16 +1,42 @@
 package practice;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class P07 {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         // Navigate to website  https://testpages.herokuapp.com/styled/index.html
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
         // Under the  ORIGINAL CONTENTS click on Alerts
+        driver.findElement(By.xpath("//*[@id='alerts']")).click();
         // print the URL
+        String actualURL= driver.getCurrentUrl();
+        System.out.println("Alerts Page URL = " + actualURL);
         // navigate back
+        driver.navigate().back();
         // print the URL
+        System.out.println("webPageCurrentUrl = " + driver.getCurrentUrl());
         // Click on Basic Ajax
+        driver.findElement(By.xpath("//*[@id='basicajax']")).click();
         // print the URL
+        System.out.println("Basic Ajax Url= "+driver.getCurrentUrl());
         // enter value-> 20 and Enter
+        driver.findElement(By.id("lteq30")).sendKeys("20", Keys.ENTER);
         // and then verify Submitted Values is displayed open page
+        WebElement submitText = driver.findElement(By.xpath("//p[text()='Submitted Values']"));
+        Assert.assertTrue(submitText.isDisplayed());
         // close driver
+        driver.close();
     }
 }
