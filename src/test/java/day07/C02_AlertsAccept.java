678f8dc5b0202617ccb5a5058ac4701c2cbc101c
Sumeyye <sumeyyedagdevirozmen@gmail.com>
2022-12-29 21:48:05 +0000

    29/12/22

--- src/test/java/day07/C02_AlertsAccept.java
+++ src/test/java/day07/C02_AlertsAccept.java
@@ -1,12 +1,12 @@
 package day07;
 
-<<<<<<<<< Temporary merge branch 1
+
 import org.junit.Test;
-=========
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
->>>>>>>>> Temporary merge branch 2
+
 import utilities.TestBase;
 
 public class C02_AlertsAccept extends TestBase {
@@ -21,17 +21,17 @@ public class C02_AlertsAccept extends TestBase {
 3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna tıklayın ve result mesajında isminizin görüntülendiğini doğrulayın.
 
      */
-    @Test
-<<<<<<<<< Temporary merge branch 1
-    public void acceptAlertTest() {
+
+
+
 
         //        1. butona tıklayın,
         //        uyarıdaki OK butonuna tıklayın
         //        ve result mesajının
         //       “You successfully clicked an alert” oldugunu test edin.
-    }
-}
-=========
+
+
+    @Test
     public void acceptAlertTest() throws InterruptedException {
         // https://testcenter.techproeducation.com/index.php?page=javascript-alerts  adresine gidin.
         driver.get("https://testcenter.techproeducation.com/index.php?page=javascript-alerts");
@@ -55,4 +55,4 @@ public class C02_AlertsAccept extends TestBase {
 Alertleri nasil automate edersin? How to handle alerts in Selenium?
 Alertler Javascript ile olusur. Inspect edilemezler. Oncelikle alert'u switch etmemiz gerekir.
  */
->>>>>>>>> Temporary merge branch 2
+
